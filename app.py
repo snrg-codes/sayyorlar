b8e915107e5c5ff15dea3cbb62e1f18cc9de11ce
khamraevs <210115349+snrgtemp@users.noreply.github.com>
2025-05-07 13:14:48 +0500

    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from functions import *
 from db import sayyoralar
 
@@ -8,5 +8,27 @@ app = Flask(__name__)
 def index():
     sayyoralar_ = sayyoralar_ruyhati(sayyoralar)
     return render_template("index.html", sayyoralar=sayyoralar_)
+
+@app.route("/sayyora/<sayyora>")
+def sayyora_info(sayyora):
+    info = sayyora_haqida(sayyoralar, sayyora)
+    return render_template("sayyora.html", info=info)
+
+@app.route("/yangi_sayyora", methods=["GET", "POST"])
+def yangi_sayyora():
+    if request.method == "POST":
+        sayyora_nomi = request.form.get("sayyora_nomi")
+        sayyora_joylashuvi = request.form.get("sayyora_joylashuvi")
+        sayyora_haqida = request.form.get("sayyora_haqida")
+        yangi_sayyora_qushish(sayyoralar, sayyora_nomi, sayyora_joylashuvi, sayyora_haqida)
+
+    return render_template("yangi_sayyora.html")
+
+
+@app.route("/sayyora_uchirish/<sayyora_id>")
+def sayyora_uchirish(sayyora_id):
+    sayyora_id = int(sayyora_id)-1
+    info = del_sayyora(sayyoralar, sayyora_id)
+    return redirect(url_for("index"))
 
 app.run(debug=True)

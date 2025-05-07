b5bdaae848b91ba495cc59d3fdc21327c0caa5f5
khamraevs <210115349+snrgtemp@users.noreply.github.com>
2025-05-07 13:10:20 +0500

    salom

--- db.py
+++ db.py
@@ -10,6 +10,30 @@ mars = {
     "sayyora_haqida": "Mars qizil sayyora"
 }
 
-sayyoralar = [yer, mars]
+yupiter = {
+    "sayyora_nomi": "Yupiter",
+    "sayyora_joylashuvi": "oxirgi o'rinda",
+    "sayyora_haqida": "Yupiter eng chetki sayyora"
+}
+
+saturn = {
+    "sayyora_nomi": "Saturn",
+    "sayyora_joylashuvi": "Belbog'li sayyora",
+    "sayyora_haqida": "Eng ko'p yo'ldoshlarga ega sayyora"
+}
+
+uran = {
+    "sayyora_nomi": "Uran",
+    "sayyora_joylashuvi": "Belbog'li sayyora",
+    "sayyora_haqida": "Eng ko'p yo'ldoshlarga ega sayyora"
+}
+
+neptun = {
+    "sayyora_nomi": "Neptun",
+    "sayyora_joylashuvi": "Belbog'li sayyora",
+    "sayyora_haqida": "Eng ko'p yo'ldoshlarga ega sayyora"
+}
+
+sayyoralar = [yer, mars, yupiter, saturn, uran, neptun]
 
 # print(sayyoralar)
